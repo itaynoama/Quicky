ca94638eb299096aece2919a30790c563477a5cb
Or <orbenda1905@gmail.com>
2016-06-08 10:33:17 +0300

    angularAdmin

--- app/public/js/appController.js
+++ app/public/js/appController.js
@@ -1,15 +1,31 @@
 var stepsApp = angular.module('quicky',[]);
 
-stepsApp.run(function($http) {
+/*stepsApp.run(function($http) {
     $http.get("http://localhost:3000/admin/getUnmodified").success(function(data) {
         model.recipes = data;
     });
-});
+});*/
 
 stepsApp.controller('quickyCtrl', function($scope){
     $scope.steps = [];
+	$scope.preparation = [];
+	$scope.cooking = [];
+	//var preparation = [];
+	//var cooking = [];
+	//$scope.steps.push(preparation);
+	//$scope.steps.push(cooking);
+	$scope.name = "Or";
 
-    $scope.addNewItem = function (actionText, actionTime) {
-        $scope.steps.push({action: actionText, time: actionTime});
+    $scope.addPreparationSteps = function (prepare, prepTime) {
+        $scope.preparation.push({action: prepare, time: prepTime});
     };
+
+	$scope.addCookingSteps = function (toCook, cookTime) {
+        $scope.cooking.push({action: toCook, time: cookTime});
+	};
+
+	$scope.setSteps = function() {
+		$scope.steps.push($scope.preparation);
+		$scope.steps.push($scope.cooking);
+	}
 });
