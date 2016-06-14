9582a2f59103e26cf6de460b5ef89a852c5861b2
itaynoama <itay.noama@gmail.com>
2016-06-15 00:12:12 +0300

    home client - range

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -1,11 +1,15 @@
-$(document).ready(function() {
-	loadHomePage();
-});
+//$(document).ready(function() {
+//	loadHomePage();
+//});
+//
+//function loadHomePage() {
+//	$.get("ajax/home.html", function(data) {
+//		var body = $('body');
+//		body.empty();
+//		body.append(data);
+//	});
+//}
 
-function loadHomePage() {
-	$.get("ajax/home.html", function(data) {
-		var body = $('body');
-		body.empty();
-		body.append(data);
-	});
+function show_value(x) {
+    document.getElementById("slider_value").innerHTML=x;
 }
